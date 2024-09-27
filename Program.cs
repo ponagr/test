e6318317370dd47c245482df14942f4f384dd725
Pontus Ågren Grundström <pontus.agrengrundstrom@gmail.com>
2024-09-27 10:48:42 +0200

    Added Book-class and Print-method

--- Program.cs
+++ Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, From GitHub");
+﻿using System.Reflection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var book1 = new Book("Harry Potter", "J.K Rowling");
+        book1.Print();
+    }
+}
+
+class Book
+{   
+    string title;
+    string author;
+    
+    public Book(string title, string author)
+    {
+        this.title = title;
+        this.author = author;
+    }
+
+    public void Print()
+    {
+        Console.Write($"Title: {title}, Author: {author}");
+    }
+}
